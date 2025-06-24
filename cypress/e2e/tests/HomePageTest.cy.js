5f8d6a22b8aa334ac0f1871f0425e5daf2a576fe
Gayathri190598 <rgayu1998@gmail.com>
2025-06-24 21:32:02 +0530

    Update HomePageTest.cy.js

--- cypress/e2e/tests/HomePageTest.cy.js
+++ cypress/e2e/tests/HomePageTest.cy.js
@@ -5,9 +5,13 @@ import TestData from "../../fixtures/TestData.json";
 
 describe("Add To Cart", ()=>{
 
-    before( ()=>{
-        cy.login(TestData.login.email , TestData.login.password)
-    })
+   before(() => {
+    cy.login(TestData.login.email, TestData.login.password);
+
+    // Wait for something that only appears when logged in
+    cy.contains('My Account', { timeout: 10000 }).should('be.visible');
+});
+
 
     it("add to cart test", ()=>{
         homeObj.enterSearchInput(TestData.product.searchInput);
